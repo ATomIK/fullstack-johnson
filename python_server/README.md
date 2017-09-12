8aeb6ed068966291264f2bae18a68f682030a1cc
ATomIK <tom@thomasj.me>
2017-09-12 10:11:55 -0500

    finished

--- python_server/README.md
+++ python_server/README.md
@@ -1,5 +1,1 @@
-#### to do:
-
- - cloudflare; ip to https://dev.thomasj.me via srv record to hide ip and port for public view.
- - python server auto start in ubuntu
- - learn how to live reload an app file instead of restarting server
+done
--- python_server/app.py
+++ python_server/app.py
@@ -1,21 +1,32 @@
-'''
+from flask import Flask, render_template, request, url_for, jsonify
+from flask_cors import CORS
 
-to do:
- - cloudflare; ip to https://dev.thomasj.me via srv record to hide ip and port for public view.
- - python server auto start in ubuntu
- - learn how to live reload an app file instead of restarting server
+app = Flask(__name__, template_folder="site")
+CORS(app)
 
-'''
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-from flask import Flask
-app = Flask(__name__)
+def has_no_empty_params(rule):
+	defaults = rule.defaults if rule.defaults is not None else ()
+	arguments = rule.arguments if rule.arguments is not None else ()
+	return len(defaults) >= len(arguments)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-	return "welcome to hell"
+	return render_template("index.html")
 
-@app.route("/ohgod")
-def ohgod():
-	return "ohgod"
+@app.route('/site-map', methods=['GET'])
+def site_map():
+	links = []
+	for rule in app.url_map.iter_rules():
+		if "GET" in rule.methods and has_no_empty_params(rule):
+			url = url_for(rule.endpoint, **(rule.defaults or {}))
+			if not url == '/site-map':
+				links.append((url, rule.endpoint))
+	if len(links) > 0:
+		return jsonify({"success": True,"routes":links})
+	else:
+		return jsonify({"success": False,"routes":links})
 
-app.run(host='0.0.0.0', port=9000, debug=False)
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080, debug=False)
