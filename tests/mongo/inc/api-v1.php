6ac82f655895f198cc9cf009cbf7b765034f6b76
UT-Tom <tom@thomasj.me>
2017-10-27 11:17:25 -0500

    curl might be broke

--- tests/mongo/inc/api-v1.php
+++ tests/mongo/inc/api-v1.php
@@ -6,34 +6,42 @@ $mongo = new mongo("mongo_tests", "users");
 
 $users["data"] = json_decode(json_encode($mongo->query()),1);
 
-if(empty($users["data"])){
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api/?results=35");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $results = json_decode(curl_exec($ch),1);
-  curl_close($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api/?results=35");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$results = json_decode(curl_exec($ch),1);
+curl_close($ch);
 
-  $_id = 1;
+echo json_encode($results,JSON_PRETTY_PRINT);
 
-  foreach($results["results"] as $user){
+// if(empty($users["data"])){
+//   $ch = curl_init();
+//   curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api/?results=35");
+//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//   $results = json_decode(curl_exec($ch),1);
+//   curl_close($ch);
+//
+//   $_id = 1;
+//
+//   foreach($results["results"] as $user){
+//
+//     $age = round((time()-strtotime($user["dob"]))/60/60/24/365);
+//
+//     $users["data"][] = [
+//       "_id" => $_id,
+//       "first" => $user["name"]["first"],
+//       "last" => $user["name"]["last"],
+//       "email" => $user["email"],
+//       "age" => $age,
+//       "nat" => $user["nat"]
+//     ];
+//
+//     $_id++;
+//
+//   }
+//
+//   $mongo->insert($users["data"]);
+//
+// }
 
-    $age = round((time()-strtotime($user["dob"]))/60/60/24/365);
-
-    $users["data"][] = [
-      "_id" => $_id,
-      "first" => $user["name"]["first"],
-      "last" => $user["name"]["last"],
-      "email" => $user["email"],
-      "age" => $age,
-      "nat" => $user["nat"]
-    ];
-
-    $_id++;
-
-  }
-
-  $mongo->insert($users["data"]);
-
-}
-
-echo json_encode($users,JSON_PRETTY_PRINT);
+// echo json_encode($users,JSON_PRETTY_PRINT);
