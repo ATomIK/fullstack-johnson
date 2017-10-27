39a551daf5be196e349f6d0907a28ca808112de7
atomik <tom@thomasj.me>
2017-10-27 01:47:33 +0000

    updated homepage

--- tests/index.php
+++ tests/index.php
@@ -5,10 +5,11 @@
     <title>4443</title>
   </head>
   <body>
-    <h1>Projects</h1>
+    <h1>4443 Projects</h1>
     <ul>
       <li><a href="./upload/">Form Upload w/ XHR Requests</a></li>
-      <li><a href="./mongo/">Mongo Helper tests</a></li>
+      <li><a href="./mongo/">MongoDB; Randomuser.me; DataTables; User manipulation.</a></li>
     </ul>
+    <p><a href="https://github.com/ut-tom/fullstack-johnson">Github Repo</a></p>
   </body>
 </html>
--- tests/upload/api.php
+++ tests/upload/api.php
@@ -6,5 +6,6 @@ if ( 0 < $_FILES['file']['error'] ) {
     echo 'Error: ' . $_FILES['file']['error'] . '<br>';
 }
 else {
-    var_dump(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']));
+    //var_dump(move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']));
+    echo 'File upload disabled.';
 }
