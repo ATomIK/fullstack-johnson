598aa66526c7962f722bcc1b6c8f6ac452b19156
UT-Tom <tom@thomasj.me>
2017-10-29 13:39:25 -0500

    on post, add row

--- display/mongo/assets/script.js
+++ display/mongo/assets/script.js
@@ -1,24 +1,17 @@
 function checkVals(arr){
   let empty = true;
   $.each(arr, function(k,v){
-    console.log(v[0]);
+    $("#addUserAlert").fadeIn();
+    $("#addUserMsg").html("Sorry, you can't leave any of these inputs blank!");
+    empty = false;
   });
-
-  // let empty = true;
-  // $.each(arr.split('&'), function(k,v){
-  //   if(v.split('=')[1] == ""){
-  //     $("#addUserAlert").fadeIn();
-  //     $("#addUserMsg").html("Sorry, you can't leave any of these inputs blank!");
-  //     empty = false;
-  //   }
-  // });
-  // return empty;
+  return empty;
 }
 
 function getParams(str){
   let arr = new Array();
   $.each(str.split("&"), function(k,v){
-    arr.push(v.split("="));
+    arr.push(v.split("=")[1]);
   });
   return arr;
 }
@@ -52,24 +45,23 @@ $(document).ready(function(){
 
     $("#addUserAlert").fadeOut();
     let params = $("#newUser").serialize();
+    let paramArr = getParams(params);
 
-    checkVals(getParams(params));
+    if(checkParams(paramArr)){
 
-    // if(checkParams(params)){
-    //
-    //   $("#newUser").toggleClass("loading");
-    //
-    //   $.ajax({
-    //     type: "POST",
-    //     url: "./inc/api-v2.php",
-    //     data: params,
-    //     success: function(response){
-    //       $("#newUser").toggleClass("loading");
-    //       datatable.row.add(params).draw( false );
-    //       console.log(response);
-    //     }
-    //   })
-    // }
+      $("#newUser").toggleClass("loading");
+
+      $.ajax({
+        type: "POST",
+        url: "./inc/api-v2.php",
+        data: params,
+        success: function(response){
+          $("#newUser").toggleClass("loading");
+          datatable.row.add(paramArr).draw( false );
+          console.log(response);
+        }
+      })
+    }
 
   });
 
