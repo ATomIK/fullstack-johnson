8906d4191ed9dfa1d33b2fbe673cc071b83a35e9
UT-Tom <tom@thomasj.me>
2017-10-29 13:34:00 -0500

    need to change functions

--- display/mongo/assets/script.js
+++ display/mongo/assets/script.js
@@ -1,13 +1,26 @@
-function checkParams(arr){
+function checkVals(arr){
   let empty = true;
-  $.each(arr.split('&'), function(k,v){
-    if(v.split('=')[1] == ""){
-      $("#addUserAlert").fadeIn();
-      $("#addUserMsg").html("Sorry, you can't leave any of these inputs blank!");
-      empty = false;
-    }
+  $.each(arr, function(k,v){
+    console.log(v[0]);
   });
-  return empty;
+
+  // let empty = true;
+  // $.each(arr.split('&'), function(k,v){
+  //   if(v.split('=')[1] == ""){
+  //     $("#addUserAlert").fadeIn();
+  //     $("#addUserMsg").html("Sorry, you can't leave any of these inputs blank!");
+  //     empty = false;
+  //   }
+  // });
+  // return empty;
+}
+
+function getParams(str){
+  let arr = new Array();
+  $.each(str.split("&"), function(k,v){
+    arr.push(v.split("="));
+  });
+  return arr;
 }
 
 $(document).ready(function(){
@@ -40,20 +53,23 @@ $(document).ready(function(){
     $("#addUserAlert").fadeOut();
     let params = $("#newUser").serialize();
 
-    if(checkParams(params)){
+    checkVals(params);
 
-      $("#newUser").toggleClass("loading");
-
-      $.ajax({
-        type: "POST",
-        url: "./inc/api-v2.php",
-        data: params,
-        success: function(response){
-          $("#newUser").toggleClass("loading");
-          console.log(response);
-        }
-      })
-    }
+    // if(checkParams(params)){
+    //
+    //   $("#newUser").toggleClass("loading");
+    //
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "./inc/api-v2.php",
+    //     data: params,
+    //     success: function(response){
+    //       $("#newUser").toggleClass("loading");
+    //       datatable.row.add(params).draw( false );
+    //       console.log(response);
+    //     }
+    //   })
+    // }
 
   });
 
