7ba9b44cf77d0a649cc1d5a57a6c817d73c80752
UT-Tom <tom@thomasj.me>
2017-10-29 21:45:10 -0500

    _id fixed

--- display/mongo/assets/script.js
+++ display/mongo/assets/script.js
@@ -88,8 +88,6 @@ $(document).ready(function(){
         success: function(response){
           $("#newUser").toggleClass("loading");
 
-          console.log(response._id);
-
           // need to also add MID
           datatable.row.add({
             _id: response._id,
