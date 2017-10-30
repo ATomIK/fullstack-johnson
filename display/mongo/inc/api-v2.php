78fb9ed7bb7830f96bf7d9e869725eb9ed4dd2e7
UT-Tom <tom@thomasj.me>
2017-10-29 21:20:28 -0500

    somethings up with v2

--- display/mongo/inc/api-v2.php
+++ display/mongo/inc/api-v2.php
@@ -11,59 +11,59 @@ require './mongodb.helper.php';
 
 class API extends mongo {
 
-  public function __construct($db,$col){
+  public function __construct(){
 
-    $this->mongo = new mongo($db, $col);
+    $this->mongo = new mongo("mongo_tests", "users");
 
     $this->method = $_SERVER['REQUEST_METHOD'];
 
   }
 
-  // public function processApi(){
-  //
-  //   if($this->method == "POST"){
-  //
-  //     if($_POST['__method'] == "PUT"){
-  //       $newId = count($this->mongo->query())+1;
-  //       $this->mongo->insert([
-  //         [
-  //           '_id' => $newId,
-  //           'first' => $_POST['first'],
-  //           'last' => $_POST['last'],
-  //           'email' => $_POST['email'],
-  //           'age' => $_POST['age'],
-  //           'nat' => $_POST['nat']
-  //         ]
-  //       ]);
-  //       echo json_encode(["status" => true, "_id" => $newId],JSON_PRETTY_PRINT);
-  //     }
-  //
-  //     if($_POST['__method'] == "PATCH"){
-  //       $this->mongo->update(["_id" => intval($_POST['_id'])],[
-  //         'first' => $_POST['first'],
-  //         'last' => $_POST['last'],
-  //         'email' => $_POST['email'],
-  //         'age' => $_POST['age'],
-  //         'nat' => $_POST['nat']
-  //       ]);
-  //       echo json_encode(["status" => true],JSON_PRETTY_PRINT);
-  //     }
-  //
-  //     if($_POST['__method'] == "DELETE"){
-  //       $this->mongo->delete([["_id" => intval($_POST['_id'])]]);
-  //       echo json_encode(["status" => true, "_id" => $_POST['_id']],JSON_PRETTY_PRINT);
-  //     }
-  //
-  //   }
-  //
-  //   // getting data in the form of json
-  //   if($this->method == "GET"){
-  //     echo json_encode(["status" => "go away."], JSON_PRETTY_PRINT);
-  //   }
-  //
-  // }
+  public function processApi(){
+
+    if($this->method == "POST"){
+
+      if($_POST['__method'] == "PUT"){
+        $newId = count($this->mongo->query())+1;
+        $this->mongo->insert([
+          [
+            '_id' => $newId,
+            'first' => $_POST['first'],
+            'last' => $_POST['last'],
+            'email' => $_POST['email'],
+            'age' => $_POST['age'],
+            'nat' => $_POST['nat']
+          ]
+        ]);
+        echo json_encode(["status" => true, "_id" = $newId],JSON_PRETTY_PRINT);
+      }
+
+      if($_POST['__method'] == "PATCH"){
+        $this->mongo->update(["_id" => intval($_POST['_id'])],[
+          'first' => $_POST['first'],
+          'last' => $_POST['last'],
+          'email' => $_POST['email'],
+          'age' => $_POST['age'],
+          'nat' => $_POST['nat']
+        ]);
+        echo json_encode(["status" => true],JSON_PRETTY_PRINT);
+      }
+
+      if($_POST['__method'] == "DELETE"){
+        $this->mongo->delete([["_id" => intval($_POST['_id'])]]);
+        echo json_encode(["status" => true, "_id" => $_POST['_id']],JSON_PRETTY_PRINT);
+      }
+
+    }
+
+    // getting data in the form of json
+    if($this->method == "GET"){
+      echo json_encode(["status" => "go away."], JSON_PRETTY_PRINT);
+    }
+
+  }
 
 }
 
-$api = new Api("mongo_tests", "users");
-// $api->processApi();
+$api = new Api;
+$api->processApi();
