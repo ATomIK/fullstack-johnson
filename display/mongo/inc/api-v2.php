23d2fd695aec12d4be08e6fd2a028dec827521ec
UT-Tom <tom@thomasj.me>
2017-10-29 21:09:39 -0500

    still 500

--- display/mongo/inc/api-v2.php
+++ display/mongo/inc/api-v2.php
@@ -21,13 +21,13 @@ class API extends mongo {
 
   }
 
-  public function getHighest(){
-
-    $all = $this->mongo->query();
-
-    var_dump($all);
-
-  }
+  // public function getHighest(){
+  //
+  //   $all = $this->mongo->query();
+  //
+  //   var_dump($all);
+  //
+  // }
 
   public function processApi(){
 
@@ -68,7 +68,7 @@ class API extends mongo {
 
     // getting data in the form of json
     if($this->method == "GET"){
-      echo json_encode(["status" => "go away.", "all" => $this->getHighest()], JSON_PRETTY_PRINT);
+      echo json_encode(["status" => "go away."], JSON_PRETTY_PRINT);
     }
 
   }
