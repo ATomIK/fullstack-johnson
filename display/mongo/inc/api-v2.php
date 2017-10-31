2eaca4198ff93b05d5eaf5729eb88fdae4f0fec1
UT-Tom <tom@thomasj.me>
2017-10-31 02:30:28 -0500

    move code into separate methods

--- display/mongo/inc/api-v2.php
+++ display/mongo/inc/api-v2.php
@@ -19,44 +19,86 @@ class API extends mongo {
 
   }
 
+  /**
+   * add_user - create a new document
+   *
+   * @params:
+   *     n/a
+   * @returns:
+   *     n/a
+   */
+
+  public function add_user(){
+    $newId = count($this->mongo->query())+1;
+    $this->mongo->insert([
+      [
+        '_id' => $newId,
+        'first' => $_POST['first'],
+        'last' => $_POST['last'],
+        'email' => $_POST['email'],
+        'age' => $_POST['age'],
+        'nat' => $_POST['nat']
+      ]
+    ]);
+    echo json_encode(["status" => true, "_id" => $newId],JSON_PRETTY_PRINT);
+  }
+
+  /**
+   * update_user - create a new document
+   *
+   * @params:
+   *     n/a
+   * @returns:
+   *     n/a
+   */
+
+  public function update_user(){
+    $this->mongo->update(["_id" => intval($_POST['_id'])],[
+      'first' => $_POST['first'],
+      'last' => $_POST['last'],
+      'email' => $_POST['email'],
+      'age' => $_POST['age'],
+      'nat' => $_POST['nat']
+    ]);
+    echo json_encode(["status" => true],JSON_PRETTY_PRINT);
+  }
+
+  /**
+   * delete_user - create a new document
+   *
+   * @params:
+   *     n/a
+   * @returns:
+   *     n/a
+   */
+
+  public function delete_user(){
+    $this->mongo->delete([["_id" => intval($_POST['_id'])]]);
+    echo json_encode(["status" => true, "_id" => $_POST['_id']],JSON_PRETTY_PRINT);
+  }
+
+  /**
+   * processApi - listen for methods
+   *
+   * @params:
+   *     n/a
+   * @returns:
+   *     n/a
+   */
+
   public function processApi(){
 
     if($this->method == "POST"){
 
-      if($_POST['__method'] == "PUT"){
-        $newId = count($this->mongo->query())+1;
-        $this->mongo->insert([
-          [
-            '_id' => $newId,
-            'first' => $_POST['first'],
-            'last' => $_POST['last'],
-            'email' => $_POST['email'],
-            'age' => $_POST['age'],
-            'nat' => $_POST['nat']
-          ]
-        ]);
-        echo json_encode(["status" => true, "_id" => $newId],JSON_PRETTY_PRINT);
-      }
-
-      if($_POST['__method'] == "PATCH"){
-        $this->mongo->update(["_id" => intval($_POST['_id'])],[
-          'first' => $_POST['first'],
-          'last' => $_POST['last'],
-          'email' => $_POST['email'],
-          'age' => $_POST['age'],
-          'nat' => $_POST['nat']
-        ]);
-        echo json_encode(["status" => true],JSON_PRETTY_PRINT);
-      }
-
-      if($_POST['__method'] == "DELETE"){
-        $this->mongo->delete([["_id" => intval($_POST['_id'])]]);
-        echo json_encode(["status" => true, "_id" => $_POST['_id']],JSON_PRETTY_PRINT);
-      }
+      if($_POST['__method'] == "PUT")
+        $this->add_user();
+      if($_POST['__method'] == "PATCH")
+        $this->update_user();
+      if($_POST['__method'] == "DELETE")
+        $this->delete_user();
 
     }
 
-    // getting data in the form of json
     if($this->method == "GET"){
       echo json_encode(["status" => "go away."], JSON_PRETTY_PRINT);
     }
