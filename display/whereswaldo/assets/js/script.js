848db226cb901e5b6bc6b26c78153d49b4476d5f
UT-Tom <tom@thomasj.me>
2017-11-09 15:52:27 -0600

    missspelled

--- display/whereswaldo/assets/js/script.js
+++ display/whereswaldo/assets/js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
     $("#time").html(dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds()+":"+dt.getMilliseconds())
   },1);
 
-  let dt = new Date();
+  let dr = new Date();
   let nr = Math.round(dr.getTime()/1000) + 300; // add 5 minutes
   setInterval(function(){
     // no you
-    $("#timer").html(dt.getTime()/1000+" - "+nr+" = "+(dt.getTime()/1000 - nr));
+    $("#timer").html(dr.getTime()/1000+" - "+nr+" = "+(dr.getTime()/1000 - nr));
   },1);
 
 });
