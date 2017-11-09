035621010cd0f5ed216d32e59fce30b7fdecf737
UT-Tom <tom@thomasj.me>
2017-11-09 15:51:00 -0600

    idk if this will work

--- display/whereswaldo/assets/js/script.js
+++ display/whereswaldo/assets/js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
   setInterval(function(){
-    var dt = new Date();
+    let dt = new Date();
     $("#time").html(dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds()+":"+dt.getMilliseconds())
+  },1);
+
+  let dt = new Date();
+  let nr = Math.round(dr.getTime()/1000) + 300; // add 5 minutes
+  setInterval(function(){
+    // no you
+    $("#timer").html(dt.getTime()/1000+" - "+nr+" = "+(dt.getTime()/1000 - nr));
   },1);
 
 });
--- display/whereswaldo/index.php
+++ display/whereswaldo/index.php
@@ -7,7 +7,11 @@
   </head>
   <body>
 
+    <p>millisecond clock</p>
     <h1 id="time">00:00</h1>
+
+    <p>millisecond timer</p>
+    <h1 id="timer">00:00</h1>
 
     <script src="//code.jquery.com/jquery-3.2.1.min.js" charset="utf-8"></script>
     <script src="./assets/js/script.js" charset="utf-8"></script>
