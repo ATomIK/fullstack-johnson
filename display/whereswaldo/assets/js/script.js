b24f89e0e10538fe3fd85360c387f120a50a008d
UT-Tom <tom@thomasj.me>
2017-11-09 15:40:48 -0600

    oh this is ugly

--- display/whereswaldo/assets/js/script.js
+++ display/whereswaldo/assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   setInterval(function(){
     var dt = new Date();
-    $("#time").html(dt.getHours()+":"+dt.getMinutes()+":"+dt.getMilliseconds())
-  },500);
+    $("#time").html(dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds()+":"+dt.getMilliseconds())
+  },10);
 
 });
