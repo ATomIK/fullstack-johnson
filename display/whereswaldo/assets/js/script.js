ad8c9798010a8c3228e70672c4bbd4b245f2c573
UT-Tom <tom@thomasj.me>
2017-11-09 15:42:43 -0600

    FASTER

--- display/whereswaldo/assets/js/script.js
+++ display/whereswaldo/assets/js/script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
   setInterval(function(){
     var dt = new Date();
     $("#time").html(dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds()+":"+dt.getMilliseconds())
-  },10);
+  },1);
 
 });
