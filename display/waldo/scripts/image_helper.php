611ede25f74382f46a2142cab0873af3d7d9a2c5
UT-Tom <tom@thomasj.me>
2017-12-09 01:15:05 -0600

    test

--- display/waldo/scripts/image_helper.php
+++ display/waldo/scripts/image_helper.php
@@ -333,7 +333,7 @@ if ($_GET['arg'] == 'run_image_tests') {
 
     $base = $waldoGame->place_waldo('./fullstack/waldo/images/crowd.jpg', $waldoImg, 16, 32, 300, 300);
 
-    var_dump($waldoGame->save_image($base, './test_output', "asdf.jpg"));
+    var_dump($waldoGame->save_image($base, '/var/www/html/display/waldo/scripts/test_output', "asdf.jpg"));
 
     echo "done";
 
