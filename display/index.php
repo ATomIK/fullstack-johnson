19d2a04119e9e22347c545c4b781678d8e27cdc6
UT-Tom <tom@thomasj.me>
2017-12-12 14:10:50 -0600

    moving

--- display/index.php
+++ display/index.php
@@ -7,10 +7,10 @@
   <body>
     <h1>4443 Projects</h1>
     <ul>
+      <li><a href="./memegenerator">Meme generator</a></li>
+      <li><a href="./useradmin">MongoDB; Randomuser.me; DataTables; User manipulation.</a></li>
+      <li><a href="./waldo/">Where's Waldo generator</a></li>
       <li><a href="./upload/">Form Upload w/ XHR Requests</a></li>
-      <li><a href="./mongo/">MongoDB; Randomuser.me; DataTables; User manipulation.</a></li>
-      <li><a href="./whereswaldo/">Where's Waldo generator; Leaderboards</a></li>
-      <li><a href="./shop/">Shopping cart system; MongoDB</a></li>
     </ul>
     <p><a href="https://github.com/ut-tom/fullstack-johnson">Github Repo!</a></p>
   </body>
